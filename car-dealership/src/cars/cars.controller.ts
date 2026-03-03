bc211c0711826cd3ae33e33c4e1f72622d4d37df
Constantino Mora <5051080+Constantino@users.noreply.github.com>
2026-03-02 22:01:49 -0600

    baseline

---
+++ car-dealership/src/cars/cars.controller.ts
@@ -0,0 +1,39 @@
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+
+    constructor(
+        private readonly carsService: CarsService
+    ) { }
+
+    @Get()
+    getAllCars() {
+        return this.carsService.findAll();
+    }
+
+    @Get(':id')
+    getCarById(@Param('id') id: string) {
+        console.log("id: ", id);
+        return this.carsService.findOneById(id);
+    }
+
+    @Post()
+    createCar(@Body() body: any) {
+        return body;
+    }
+
+    @Patch(':id')
+    updateCar(@Param('id') id: string, @Body() body: any) {
+        return body;
+    }
+
+    @Delete(':id')
+    deleteCar(@Param('id') id: string) {
+        return {
+            ok: true,
+            method: 'DELETE',
+        };
+    }
+}
---
+++ car-dealership/src/cars/cars.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class CarsService {
+
+    private cars: Car[] = [
+        {
+            id: uuid(),
+            brand: 'Toyota',
+            model: 'Corolla',
+        },
+        {
+            id: uuid(),
+            brand: 'Ford',
+            model: 'Mustang',
+        },
+        {
+            id: uuid(),
+            brand: 'Chevrolet',
+            model: 'Camaro',
+        },
+    ];
+
+    findAll() {
+        return this.cars;
+    }
+
+    findOneById(id: string) {
+        const car = this.cars.find(car => car.id === id);
+        if (!car)
+            throw new NotFoundException(`Car with id ${id} not found`);
+
+        return car;
+    }
+
+    create(body: any) {
+        return {
+            ok: true,
+            method: 'POST',
+        };
+    }
+}
